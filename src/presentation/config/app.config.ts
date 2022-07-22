51ada51797d27903fe68b0dcfd109e587a2b7f30
Avinash Reddy <avinashreddy3108@gmail.com>
2022-07-22 17:34:01 +0530

    fix(appinfo): ran prettier

--- src/presentation/config/app.config.ts
+++ src/presentation/config/app.config.ts
@@ -1,40 +1,22 @@
-import { sync as readPackageJsonSync } from "read-pkg";
+import { sync as readPackageJsonSync } from 'read-pkg';
 
-import {
-  getEnvironmentNumber,
-  getEnvironmentString,
-} from "@infrastructure/shared/config/environment";
+import { getEnvironmentNumber, getEnvironmentString } from '@infrastructure/shared/config/environment';
 
 const AppInfo = {
-  APP_VERSION: getEnvironmentString(
-    "APP_VERSION",
-    readPackageJsonSync().version
-  ),
-  APP_NAME: getEnvironmentString("APP_NAME", readPackageJsonSync().name),
-  APP_DESCRIPTION: getEnvironmentString(
-    "APP_DESCRIPTION",
-    readPackageJsonSync().description || "N/A"
-  ),
-  AUTHOR_NAME: getEnvironmentString(
-    "AUTHOR_NAME",
-    readPackageJsonSync().author?.name || "N/A"
-  ),
-  AUTHOR_EMAIL: getEnvironmentString(
-    "AUTHOR_EMAIL",
-    readPackageJsonSync().author?.email || "N/A"
-  ),
-  AUTHOR_WEBSITE: getEnvironmentString(
-    "AUTHOR_WEBSITE",
-    readPackageJsonSync().author?.url || "N/A"
-  ),
+  APP_VERSION: getEnvironmentString('APP_VERSION', readPackageJsonSync().version),
+  APP_NAME: getEnvironmentString('APP_NAME', readPackageJsonSync().name),
+  APP_DESCRIPTION: getEnvironmentString('APP_DESCRIPTION', readPackageJsonSync().description || 'N/A'),
+  AUTHOR_NAME: getEnvironmentString('AUTHOR_NAME', readPackageJsonSync().author?.name || 'N/A'),
+  AUTHOR_EMAIL: getEnvironmentString('AUTHOR_EMAIL', readPackageJsonSync().author?.email || 'N/A'),
+  AUTHOR_WEBSITE: getEnvironmentString('AUTHOR_WEBSITE', readPackageJsonSync().author?.url || 'N/A')
 };
 
 const AppConfig = {
-  PORT: getEnvironmentNumber("PORT", 3000),
-  BASE_PATH: getEnvironmentString("BASE_PATH", "/api"),
-  JWT_SECRET: getEnvironmentString("JWT_SECRET", "jwtSecretPassphrase"),
-  JWT_EXPIRATION: getEnvironmentNumber("JWT_EXPIRATION", 1),
-  JWT_REFRESH_EXPIRATION: getEnvironmentNumber("JWT_REFRESH_EXPIRATION", 6),
+  PORT: getEnvironmentNumber('PORT', 3000),
+  BASE_PATH: getEnvironmentString('BASE_PATH', '/api'),
+  JWT_SECRET: getEnvironmentString('JWT_SECRET', 'jwtSecretPassphrase'),
+  JWT_EXPIRATION: getEnvironmentNumber('JWT_EXPIRATION', 1),
+  JWT_REFRESH_EXPIRATION: getEnvironmentNumber('JWT_REFRESH_EXPIRATION', 6)
 };
 
 export { AppConfig, AppInfo };
